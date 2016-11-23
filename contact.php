b641e20df9ced34a5c493cba6ef64854f89f1df1
jeremy <jeremy.touyon@live.fr>
2016-11-23 11:36:48 +0100

    modif de contact.php sur la branche dev

--- contact.php
+++ contact.php
@@ -7,6 +7,12 @@
   </head>
   <body>
     <h1>Contact</h1>
+    <form class="" action="index.html" method="post">
+      <label for="name">Nom</label>
+      <input type="text" name="name" value=""><br>
 
+      <label for="prenom">Prenom</label>
+      <input type="text" name="prenom" value=""><br>
+    </form>
   </body>
 </html>
