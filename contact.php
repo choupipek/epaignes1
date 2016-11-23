bcbf0e22872b86bd4ae5915862431c0036de190d
jeremy <jeremy.touyon@live.fr>
2016-11-23 10:40:23 +0100

    modification de contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,12 @@
 <?php echo "contact.php"; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>contact</title>
+  </head>
+  <body>
+    <h1>Contact</h1>
+
+  </body>
+</html>
